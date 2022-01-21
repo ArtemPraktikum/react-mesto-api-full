c33b8508b9716c6e1172d2948c4eee51a5bfadc9
Artem Velmozhin <artemvrabota@yandex.ru>
2022-01-21 17:02:48 +0400

    fix: полная переработка auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,24 +4,24 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  const authorization = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const token = req.headers.authorization.replace('Bearer ', '');
+  console.log(req.headers.authorization);
+  console.log(token);
+  if (!token) {
     throw new UnauthorizedError('Необходимо залогиниться');
+  } else {
+    let payload;
+    try {
+      payload = jwt.verify(
+        token,
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      );
+    } catch (err) {
+      throw new UnauthorizedError('Необходимо залогиниться');
+    }
+    req.user = payload;
+    next();
   }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-  } catch (err) {
-    // выкинуть ошибку если верификация токена не удалась.
-    throw new UnauthorizedError('Необходимо залогиниться');
-  }
-
-  req.user = payload;
-
-  next();
 };
 
 module.exports = auth;
